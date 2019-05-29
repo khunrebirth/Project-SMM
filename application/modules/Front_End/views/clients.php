68c494a856a79152505df1ff433629d51250a6d1
khunrebirth <khunrebirth@gmail.com>
2019-05-29 14:44:49 +0700

    init

--- application/modules/Front_End/views/clients.php
+++ application/modules/Front_End/views/clients.php
@@ -1,5 +1,5 @@
 <!-- Title -->
-<section class="page-title" style="background-image:url(http://localhost/social.co.th/resources/resources/front-end/images/slider-1.jpg)">
+<section class="page-title" style="background-image:url(http://localhost/social.co.th/resources/front-end/images/slider-1.jpg)">
     <div class="container my-auto text-center wow fadeInUp">
         <h1>Client Page</h1>
     </div>
