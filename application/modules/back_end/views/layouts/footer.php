7fba8c44ecd7cbcd66f02e64a1f252c6833c94bb
khunrebirth <khunrebirth@gmail.com>
2019-10-11 17:28:46 +0700

    update: project

--- application/modules/back_end/views/layouts/footer.php
+++ application/modules/back_end/views/layouts/footer.php
@@ -20,7 +20,40 @@
         <script src="<?php echo base_url('resources/back_end/assets/js/page/index-0.js'); ?>"></script>
 
         <!-- Custom JS -->
-        <script></script>
+		<script>
+            $(document).ready(function() {
+                $('#ssmDataTable').dataTable( {
+                    dom: 'Bfrtip',
+                    buttons: [
+                        {
+                            extend: 'copy',
+                            text: '<i class="far fa-copy"></i> Copy',
+                            className: 'btn btn-primary'
+                        },
+                        {
+                            extend: 'csv',
+                            text: '<i class="fas fa-file-csv"></i> CSV',
+                            className: 'btn btn-primary'
+                        },
+                        {
+                            extend: 'excel',
+                            text: '<i class="far fa-file-excel"></i> Excel',
+                            className: 'btn btn-primary'
+                        },
+                        {
+                            extend: 'pdf',
+                            text: '<i class="far fa-file-pdf"></i> PDF',
+                            className: 'btn btn-primary'
+                        },
+                        {
+                            extend: 'print',
+                            text: '<i class="fas fa-print"></i> Print',
+                            className: 'btn btn-primary'
+                        }
+                    ]
+                });
+            });
+		</script>
 
 </body>
 </html>
