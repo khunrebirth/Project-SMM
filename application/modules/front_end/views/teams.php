18eb92f01340334b8d8d2f42954f40b892592495
saijo_trm <teeramate.trm@gmail.com>
2019-10-17 17:50:39 +0700

    up team FE

--- application/modules/front_end/views/teams.php
+++ application/modules/front_end/views/teams.php
@@ -1,6 +1,5 @@
 <!-- Push Custom Style -->
 <style>
-<<<<<<< HEAD
 .box-slide-team {
     max-width: 1730px;
     width: 100%;
@@ -48,54 +47,6 @@
     background: url(<?php echo base_url($lang == 'th' ? 'resources/front_end/images/team/btn_arrow_right.png' : 'resources/front_end/images/team/btn_arrow_right.png'); ?>) !important;
 }
 
-=======
-	.box-slide-team {
-		max-width: 1730px;
-		width: 100%;
-		padding: 0 104px;
-		margin: 0 auto;
-	}
-	.box-slide-team .owl-nav {
-		position: absolute;
-		top: 30%;
-		-webkit-transform: translate3d(0,-50%,0);
-				transform: translate3d(0,-50%,0);
-		width: 100%;
-		left: 0;
-	}
-	.box-slide-team .owl-nav .owl-prev,
-	.box-slide-team .owl-nav .owl-next {
-		position: absolute;
-		width: 76px;
-		height: 76px;
-		background-color: #fff !important;
-		transition: all 0.3s;
-		-webkit-transition: all 0.3s;
-		-moz-transition: all 0.3s;
-		-ms-transition: all 0.3s;
-		-o-transition: all 0.3s;
-	}
-	.box-slide-team .owl-nav .owl-prev:focus,
-	.box-slide-team .owl-nav .owl-next:focus {
-		outline: none;
-	}
-	.box-slide-team .owl-nav .owl-prev:hover,
-	.box-slide-team .owl-nav .owl-next:hover {
-		opacity: 0.7;
-	}
-	.box-slide-team .owl-nav .owl-prev span,
-	.box-slide-team .owl-nav .owl-next span {
-		display: none;
-	}
-	.box-slide-team .owl-nav .owl-prev {
-		left: 0;
-		background: url(<?php echo base_url($lang == 'th' ? 'resources/front_end/images/team/btn_arrow_left.png' : 'resources/front_end/images/team/btn_arrow_left.png'); ?>) !important;
-	}
-	.box-slide-team .owl-nav .owl-next {
-		right: 0;
-		background: url(<?php echo base_url($lang == 'th' ? 'resources/front_end/images/team/btn_arrow_right.png' : 'resources/front_end/images/team/btn_arrow_right.png'); ?>) !important;
-	}
->>>>>>> c10c3379e95b1adc1631450a958a03a8d3956340
 </style>
 
 <!-- Title -->
