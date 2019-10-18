cbb400f3a991536b50fc9bc1d754145b10f4d468
saijo_trm <teeramate.trm@gmail.com>
2019-10-18 13:20:01 +0700

    add thnank_you page

---
+++ application/modules/front_end/views/thank_you.php
@@ -0,0 +1,46 @@
+<style>
+	.box-thank-you {
+		max-width: 1440px;
+		width: 100%;
+		margin: 0 auto;
+		padding: 150px 0
+	}
+	.thank-you-ttl span {
+		color: #0d84be;
+	}
+	.thank-you-txt {
+		padding: 0 15px;
+	}
+	.thank-you-img {
+		padding: 0 30px;
+	}
+</style>
+
+<!-- Title -->
+<section class="page-title" style="background-image:url(http://localhost/social-dev/resources/front_end/images/slider-1.jpg)">
+	<div class="container my-auto text-center wow fadeInUp">
+		<h1>THANK YOU</h1>
+	</div>
+</section>
+
+<!-- Content -->
+<section class="sec-thank-you">
+	<div class="box-thank-you row">
+		<div class="col-md-6 col-lg-6 text-right pr-3 pr-xs-0 text-center text-lg-right  text-sm-center">
+			<h2 class="thank-you-ttl mt-md-0 mt-4"><span>T</span>hank you</h2>
+			<p class="lead thank-you-txt mt-4 mb-4">
+			โครนาโมเต็ล บอยคอตเมจิคสแตนดาร์ดกฤษณ์ ศึกษาศาสตร์ออร์แกนิคสเตย์เวเฟอร์ อะป๊อปอยุติธรรม อมาตยาธิปไตยตื้บโพสต์ทัวริสต์ เพลซอัลบั้มรีสอร์ตแฟรี 
+			หยวนแฮมเบอร์เกอร์ คอนโดคาแร็คเตอร์ยังไงป่าไม้มอคคา ยอมรับ
+			</p>
+		</div>
+		<div class="col-xs-12 col-md-6 col-lg-6 pl-md-5 text-left text-lg-left text-sm-center  thank-you-img">
+			<img src="<?php echo base_url('resources/front_end/images/img-/images/title-service.png')?>" alt="" class="img-fluid">
+			<div class="col text-left mt-5">
+				<button type="submit" class="btn btn-primary text-center col-lg-6 col-sm-12">กลับไปยังหน้าแรก</button>
+			</div>
+		</div>
+	</div>
+</section>
+
+<!-- SiteMap -->
+<?php include 'layouts/sitemap.php'; ?>
