6aa0620d5b5f8f958fcbc766a6cd1b129e5fd524
khunrebirth <khunrebirth@gmail.com>
2019-10-18 11:27:47 +0700

    update: project

---
+++ application/modules/front_end/controllers/Thank_you.php
@@ -0,0 +1,73 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Thank_you extends MX_Controller
+{
+
+	private $lang = 'th';
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		/*
+		| -------------------------------------------------------------------------
+		| SET UTILITIES
+		| -------------------------------------------------------------------------
+		*/
+
+		// Model
+		$this->load->model('Home_page_model');
+
+		// Language
+		$this->lang = $this->config->item('language_abbr');
+	}
+
+	public function index()
+	{
+
+		/*
+		| -------------------------------------------------------------------------
+		| HANDLE
+		| -------------------------------------------------------------------------
+		*/
+
+		$home_id = 1;
+		$page_content = $this->Home_page_model->get_home_page_by_id($home_id);
+
+		/*
+		| -------------------------------------------------------------------------
+		| SET DATA
+		| -------------------------------------------------------------------------
+		*/
+
+		// Language
+		$data['lang'] = $this->lang;
+
+		// Title Page
+		$data['title'] = unserialize($page_content->meta_tag_title)[$this->lang];
+
+		// Meta Tag
+		$data['meta']['title'] = unserialize($page_content->meta_tag_title)[$this->lang];
+		$data['meta']['description'] = unserialize($page_content->meta_tag_description)[$this->lang];
+		$data['meta']['keyword'] = unserialize($page_content->meta_tag_keywords)[$this->lang];
+
+		// OG & Twitter
+		$data['og_twitter']['title'] = unserialize($page_content->meta_tag_title)[$this->lang];
+		$data['og_twitter']['description'] = unserialize($page_content->meta_tag_description)[$this->lang];
+		$data['og_twitter']['image'] = base_url('storage/uploads/images/home/'. unserialize($page_content->img_og_twitter)[$this->lang]);
+
+		// Content
+		$data['content'] = 'thank_you';
+
+		// Utilities
+
+		/*
+		| -------------------------------------------------------------------------
+		| EXECUTE VIEWS
+		| -------------------------------------------------------------------------
+		*/
+
+		$this->load->view('app', $data);
+	}
+}
