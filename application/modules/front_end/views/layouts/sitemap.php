054854a8a370055f57497db480c093706a25559e
saijo_trm <teeramate.trm@gmail.com>
2019-10-24 17:44:24 +0700

    add

--- application/modules/front_end/views/layouts/sitemap.php
+++ application/modules/front_end/views/layouts/sitemap.php
@@ -48,21 +48,13 @@
 		margin-bottom: 20px;
 	}
 </style>
-<div id="accordion">
-	<div class="card text-center" style="background: #00264c; color: #fff; border-radius: 0;">
-		<div class="card-header" id="headingOne" style="padding-top: 0px; padding-bottom: 0px;">
-			<h5 class="mb-0">
-				<button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true"
-						aria-controls="collapseOne" style="color: #fff;">
-					Site map <i class="fas fa-plus-circle"></i>
-				</button>
-			</h5>
-		</div>
-		<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+<div class="sec-footer-sitemap" id="accordion">
+	<div class="card text-center" style="background: #00264c; color: #fff; border-radius: 0;margin:0;">
+		<div class="show">
 			<div class="card-body">
 				<div class="sec-sitemap">
 					<div class="row">
-						<div class="col-sm-12 col-lg-3 mb-3 mb-md-0">
+						<div class="col-sm-12 sec-site-link col-lg-3 mb-3 mb-md-0">
 							<a class="d-block text-left" href="<?php echo base_url($lang . '/' . lang('menu_home')); ?>"><?php echo lang('menu_home'); ?></a>
 							<a class="d-block text-left" href="<?php echo base_url($lang . '/' . lang('menu_about')); ?>"><?php echo lang('menu_about'); ?></a>
 							<a class="d-block text-left" href=""><?php echo lang('menu_service'); ?></a>
@@ -77,7 +69,6 @@
 								<?php foreach ($services as $service) { ?>
 									<li><a class="text-left" href="<?php echo base_url($lang . '/' . lang('menu_service') . '/' . unserialize($service->slug)[$lang] . '/' . hashids_encrypt($service->id)); ?>"><?php echo unserialize($service->title)[$lang]; ?></a></li>
 								<?php } ?>
-								
 							</ul>
 							<a class="d-block text-left" href="<?php echo base_url($lang . '/' . lang('menu_our_clients')); ?>"><?php echo lang('menu_our_clients'); ?></a>
 							<a class="d-block text-left" href="<?php echo base_url($lang . '/' . lang('menu_blogs')); ?>"><?php echo lang('menu_blogs'); ?></a>
@@ -88,16 +79,17 @@
 						<div class="col-sm-12 col-lg-9">
 							<div class="row">
 								<div class="col-12 mb-4">
-									<img class="img-fluid" src="http://localhost/social-dev/resources/front_end/images/title-about.png" alt="">
+									<h2 class="ttl-facebook">Our Facebook Page</h2>
+									<!-- <img class="img-fluid" src="http://localhost/social-dev/resources/front_end/images/title-about.png" alt=""> -->
 								</div>
 								<div class="col-lg-4 item-fb-page">
-									<div class="fb-page" data-href="https://www.facebook.com/facebook" data-tabs="timeline" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/facebook" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote></div>
+									<div class="fb-page" data-href="https://www.facebook.com/iSEO.SEM/" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false"><blockquote cite="https://www.facebook.com/iSEO.SEM/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/iSEO.SEM/">รับบริการทำ SEO  SEM</a></blockquote></div>
 								</div>
 								<div class="col-lg-4 item-fb-page">
-									<div class="fb-page" data-href="https://www.facebook.com/facebook" data-tabs="timeline" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/facebook" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote></div>
+									<div class="fb-page" data-href="https://www.facebook.com/SocialMediaMarketing/" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false"><blockquote cite="https://www.facebook.com/SocialMediaMarketing/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/SocialMediaMarketing/">Social Media Master</a></blockquote></div>
 								</div>
 								<div class="col-lg-4 item-fb-page">
-									<div class="fb-page" data-href="https://www.facebook.com/facebook" data-tabs="timeline" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/facebook" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote></div>
+									<div class="fb-page" data-href="https://www.facebook.com/EmailDirectMarketing.EDM/" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false"><blockquote cite="https://www.facebook.com/EmailDirectMarketing.EDM/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/EmailDirectMarketing.EDM/">บริการรับส่งอีเมล์ : Email Direct Marketing</a></blockquote></div>
 								</div>
 							</div>
 						</div>
