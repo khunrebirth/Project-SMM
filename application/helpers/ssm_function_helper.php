7b9784e3889ef436c9220e20a005f10adc7010d8
khunrebirth <khunrebirth@gmail.com>
2019-10-29 14:19:13 +0700

    ีupdate: project

--- application/helpers/ssm_function_helper.php
+++ application/helpers/ssm_function_helper.php
@@ -5,3 +5,7 @@ function smm_slug($text) {
 
 	return str_replace(" ","-", $textlower);
 }
+
+function ssm_is_mobile() {
+	return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
+}
