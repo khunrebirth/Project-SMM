fe8c3a015e6c683b0070b7097c0eaaaeaf60005a
saijo_trm <teeramate.trm@gmail.com>
2019-10-30 09:48:56 +0700

    add

--- application/modules/front_end/views/join_us.php
+++ application/modules/front_end/views/join_us.php
@@ -1,106 +1,6 @@
 <!-- Push Custom Style -->
 <style>
-.box-slide-team {
-    max-width: 1730px;
-    width: 100%;
-    padding: 0 30px;
-    margin: 0 auto;
-}
 
-.box-slide-team .owl-nav {
-    position: absolute;
-    top: 30%;
-    -webkit-transform: translate3d(0,-50%,0);
-            transform: translate3d(0,-50%,0);
-    width: 100%;
-    left: 0;
-}
-
-.box-slide-team .owl-nav .owl-prev, 
-.box-slide-team .owl-nav .owl-next {
-    position: absolute;
-    width: 45px;
-    height: 45px;
-    background-color: #fff !important;
-    transition: all 0.3s;
-    -webkit-transition: all 0.3s;
-    -moz-transition: all 0.3s;
-    -ms-transition: all 0.3s;
-    -o-transition: all 0.3s;
-    
-}
-
-.box-slide-team .owl-nav .owl-prev:focus, 
-.box-slide-team .owl-nav .owl-next:focus {
-    outline: none;
-}
-
-.box-slide-team .owl-nav .owl-prev:hover, 
-.box-slide-team .owl-nav .owl-next:hover {
-    opacity: 0.7;
-}
-
-.box-slide-team .owl-nav .owl-prev span, 
-.box-slide-team .owl-nav .owl-next span {
-    display: none;
-}
-
-.box-slide-team .owl-nav .owl-prev {
-    left: 0;
-    background: url(<?php echo base_url($lang == 'th' ? 'resources/front_end/images/team/btn_arrow_left.png' : 'resources/front_end/images/team/btn_arrow_left.png'); ?>) !important;
-    background-size: cover !important;
-}
-
-.box-slide-team .owl-nav .owl-next {
-    right: 0;
-    background: url(<?php echo base_url($lang == 'th' ? 'resources/front_end/images/team/btn_arrow_right.png' : 'resources/front_end/images/team/btn_arrow_right.png'); ?>) !important;
-    background-size: cover !important;
-}
-.team__card--parent .is-hover {
-	position: absolute;
-	top: 0;
-	left: 0;
-	visibility: hidden;
-	opacity: 0;
-	transition: all 0.2s;
-	-webkit-transition: all 0.2s;
-	-moz-transition: all 0.2s;
-	-ms-transition: all 0.2s;
-	-o-transition: all 0.2s;
-}
-.team__card--parent:hover .is-hover { 
-	visibility: visible;
-	opacity: 1;
-}
-
-@media (min-width: 576px) { 
-	.box-slide-team {
-		padding: 0 30px !important;
-	}
-}
-
-@media (min-width: 768px) { 
-	.box-slide-team {
-		padding: 0 50px !important;
-    }
-    .box-slide-team .owl-nav .owl-prev, 
-    .box-slide-team .owl-nav .owl-next {
-        width: 76px;
-        height: 76px;
-    }
-}
-
-@media (min-width: 992px) { 
-	.box-slide-team {
-		padding: 0 80px !important;
-	}
-}
-
-@media (min-width: 1200px) { 
-	.box-slide-team {
-		padding: 0 140px !important;
-	}
-}
 </style>
 
 <!-- Title -->
@@ -114,31 +14,70 @@
 <?php } ?>
 
 <!-- Content -->
-<section id="team">
+<section id="job">
     <div class="container-fluid ">
         <div class="title-team" style="text-align: center; padding: 4rem;">
 			<img src="<?php echo base_url($lang == 'th' ? 'resources/front_end/images/title-team.png' : 'resources/front_end/images/title-team.png'); ?>" alt="" class="img-fluid">
+        </div>  
+        <div class="row sec-job sec-job-info mb-5">
+            <div class="col-sm-6">
+                <p> 
+                    We’re Omisians. We’re firm believers in the idea that “payment acceptance” 
+                    is fundamental to developing a successful business. We build tools and provide 
+                    services that power more than just payments; we help our clients to reach 
+                    more buyers around the world, unlocking their full potential.
+                </p>
+                <p> 
+                    We’re Omisians. We’re firm believers in the idea that “payment acceptance” 
+                    is fundamental to developing a successful business. We build tools and provide 
+                    services that power more than just payments; we help our clients to reach 
+                    more buyers around the world, unlocking their full potential.
+                </p>
+            </div>
+            <div class="col-sm-6 wrap-video">
+                <iframe width="560" height="349" src="https://www.youtube.com/embed/keqd4RUQK4s" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
         </div>
-        
-        <div class="owl-carousel owl-theme box-slide-team" id="box-slide-team">
-			<?php foreach ($teams as $team) { ?>
-				<?php
-					$fullname = unserialize($team->title)[$lang];
-					$posBlankSpace = strpos(unserialize($team->title)[$lang], " ");
-					$first_name = substr($fullname, 0, $posBlankSpace);
-					$last_name = substr($fullname, $posBlankSpace, strlen($fullname));
-				?>
-				<div>
-					<div class="card team__card--parent mx-auto" >
-						<img class="card-img-top" src="<?php echo base_url('storage/uploads/images/teams/' . unserialize($team->image)[$lang]); ?>" alt="">
-						<img class="card-img-top is-hover" src="<?php echo base_url('storage/uploads/images/teams/' . unserialize($team->image_hover)[$lang]); ?>" alt="">
-						<div class="card-body pl-0">
-							<h5 class="card-title team__crad--title"><?php echo $first_name; ?> <br><?php echo $last_name; ?></h5>
-							<p class="card-text team__crad--body"><?php echo unserialize($team->body)[$lang]; ?></p>
-						</div>
-					</div>
-				</div>
-            <?php } ?>
+        <div class="row sec-job sec-job-activity">
+            <div class="col-6 col-md-4 item-activity d-flex justify-content-center flex-column">
+                <img src="https://dummyimage.com/100x100/b8b8b8/ffffff.jpg" alt="">
+                <p>dummy text</p>
+            </div>
+            <div class="col-6 col-md-4 item-activity d-flex justify-content-center flex-column">
+                <img src="https://dummyimage.com/100x100/b8b8b8/ffffff.jpg" alt="">
+                <p>dummy text</p>
+            </div>
+            <div class="col-6 col-md-4 item-activity d-flex justify-content-center flex-column">
+                <img src="https://dummyimage.com/100x100/b8b8b8/ffffff.jpg" alt="">
+                <p>dummy text</p>
+            </div>
+            <div class="col-6 col-md-4 item-activity d-flex justify-content-center flex-column">
+                <img src="https://dummyimage.com/100x100/b8b8b8/ffffff.jpg" alt="">
+                <p>dummy text</p>
+            </div>
+            <div class="col-6 col-md-4 item-activity d-flex justify-content-center flex-column">
+                <img src="https://dummyimage.com/100x100/b8b8b8/ffffff.jpg" alt="">
+                <p>dummy text</p>
+            </div>
+            <div class="col-6 col-md-4 item-activity d-flex justify-content-center flex-column">
+                <img src="https://dummyimage.com/100x100/b8b8b8/ffffff.jpg" alt="">
+                <p>dummy text</p>
+            </div>
+        </div>
+        <div class="row sec-job sec-job-img">
+            <img src="" alt="">
+        </div>
+        <div class="row sec-job sec-job-item">
+            <ul class="list-cmn-job col-12">
+                <li class="mb-2">
+                    <a class="col-12" href="">
+                        <div class="txt">
+                            <p class="ttl"></p>
+                        </div>
+                        <span>apply</span>
+                    </a>
+                </li>
+            </ul>
         </div>
     </div>
 </section>
@@ -149,26 +88,6 @@
 <!-- Push Custom Scripts -->
 <script>
     $(function () {
-        $('#box-slide-team').owlCarousel({
-            items:5,
-            nav:true,
-            loop:true,
-            dots:false,
-            margin:15,
-            responsive : {
-                0 : {
-                    items:1,
-                },
-                576 : {
-                    items:2,
-                },
-                768 : {
-                    items:3,
-                },
-                1200 : {
-                    items:5,
-                }
-            }
-        });
+       
     });
 </script>
