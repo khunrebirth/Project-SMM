495b3cce5249b11b4be59b99bb0bb279f38381a9
saijo_trm <teeramate.trm@gmail.com>
2019-10-30 11:06:00 +0700

    add job page

--- application/modules/front_end/controllers/Job.php
+++ application/modules/front_end/controllers/Job.php
@@ -73,4 +73,54 @@ class Job extends MX_Controller
 
 		$this->load->view('app', $data);
 	}
+	
+	public function job()
+	{
+
+		/*
+		| -------------------------------------------------------------------------
+		| HANDLE
+		| -------------------------------------------------------------------------
+		*/
+
+		$team_id = 1;
+		$page_content = $this->Team_page_model->get_team_page_by_id($team_id);
+
+		/*
+		| -------------------------------------------------------------------------
+		| SET DATA
+		| -------------------------------------------------------------------------
+		*/
+
+		// Language
+		$data['lang'] = $this->lang;
+
+		// Title Page
+		$data['title'] = unserialize($page_content->meta_tag_title)[$this->lang];
+
+		// Meta Tag
+		$data['meta']['title'] = unserialize($page_content->meta_tag_title)[$this->lang];
+		$data['meta']['description'] = unserialize($page_content->meta_tag_description)[$this->lang];
+		$data['meta']['keyword'] = unserialize($page_content->meta_tag_keywords)[$this->lang];
+
+		// OG & Twitter
+		$data['og_twitter']['title'] = unserialize($page_content->meta_tag_title)[$this->lang];
+		$data['og_twitter']['description'] = unserialize($page_content->meta_tag_description)[$this->lang];
+		$data['og_twitter']['image'] = base_url('storage/uploads/images/teams/'. unserialize($page_content->img_og_twitter)[$this->lang]);
+
+		// Content
+		$data['content'] = 'job';
+
+		// // Utilities
+		// $data['teams'] = $this->Team_model->get_team_all();
+		$data['banner'] = $this->Banner_model->get_banner_active_by_id(4);
+
+		/*
+		| -------------------------------------------------------------------------
+		| EXECUTE VIEWS
+		| -------------------------------------------------------------------------
+		*/
+
+		$this->load->view('app', $data);
+	}
 }
