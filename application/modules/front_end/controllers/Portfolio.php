ea7ef4aa50285bf639458c79bffed782c5e290fc
khunrebirth <khunrebirth@gmail.com>
2019-11-06 15:55:55 +0700

    update: project

--- application/modules/front_end/controllers/Portfolio.php
+++ application/modules/front_end/controllers/Portfolio.php
@@ -17,10 +17,10 @@ class Portfolio extends MX_Controller
 		*/
 
 		// Model
-		$this->load->model('Client_page_model');
+		$this->load->model('Page_model');
+		$this->load->model('Banner_model');
 		$this->load->model('Client_category_model');
 		$this->load->model('Client_model');
-		$this->load->model('Banner_model');
 
 		// Language
 		$this->lang = $this->config->item('language_abbr');
@@ -29,15 +29,13 @@ class Portfolio extends MX_Controller
 	public function index()
 	{
 
-
 		/*
 		| -------------------------------------------------------------------------
 		| HANDLE
 		| -------------------------------------------------------------------------
 		*/
 
-		$client_id = 1;
-		$page_content = $this->Client_page_model->get_client_page_by_id($client_id);
+		$page_content = $this->Page_model->get_page_by_id(11);
 
 		/*
 		| -------------------------------------------------------------------------
@@ -50,11 +48,15 @@ class Portfolio extends MX_Controller
 
 		// Title Page
 		$data['title'] = unserialize($page_content->meta_tag_title)[$this->lang];
+		$data['title_moblie'] = unserialize($page_content->meta_tag_moblie_title)[$this->lang];
 
 		// Meta Tag
 		$data['meta']['title'] = unserialize($page_content->meta_tag_title)[$this->lang];
 		$data['meta']['description'] = unserialize($page_content->meta_tag_description)[$this->lang];
 		$data['meta']['keyword'] = unserialize($page_content->meta_tag_keywords)[$this->lang];
+		$data['meta']['title_moblie'] = unserialize($page_content->meta_tag_moblie_title)[$this->lang];
+		$data['meta']['description_moblie'] = unserialize($page_content->meta_tag_moblie_description)[$this->lang];
+		$data['meta']['keyword_moblie'] = unserialize($page_content->meta_tag_moblie_keywords)[$this->lang];
 
 		// OG & Twitter
 		$data['og_twitter']['title'] = unserialize($page_content->meta_tag_title)[$this->lang];
