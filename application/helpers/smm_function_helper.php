18d6190ce9109fa3d67922cd3968cb8eadd7408e
khunrebirth <khunrebirth@gmail.com>
2019-11-14 14:41:56 +0700

    update: project

--- application/helpers/smm_function_helper.php
+++ application/helpers/smm_function_helper.php
@@ -84,3 +84,21 @@ function smm_img_covert_to_webp($path_file) {
 
 	WebPConvert::convert($source, $destination, $options);
 }
+
+
+function get_browser_name($user_agent)
+{
+	if (strpos($user_agent, 'Opera') || strpos($user_agent, 'OPR/')) return 'Opera';
+	elseif (strpos($user_agent, 'Edge')) return 'Edge';
+	elseif (strpos($user_agent, 'Chrome')) return 'Chrome';
+	elseif (strpos($user_agent, 'Safari')) return 'Safari';
+	elseif (strpos($user_agent, 'Firefox')) return 'Firefox';
+	elseif (strpos($user_agent, 'MSIE') || strpos($user_agent, 'Trident/7')) return 'Internet Explorer';
+
+	return 'Other';
+}
+
+function is_safari()
+{
+	return get_browser_name($_SERVER['HTTP_USER_AGENT'] == 'Safari') ? true : false;
+}
